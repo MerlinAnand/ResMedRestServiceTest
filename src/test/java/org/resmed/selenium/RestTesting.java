fb9a77745117d59cbc521025877f50e451e1c97c
Merlin <merlinanand2000@gmail.com>
2017-09-12 22:46:22 +1000

    RestService Test both GET and POST

---
+++ src/test/java/org/resmed/selenium/RestTesting.java
@@ -0,0 +1,84 @@
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
+
+public class RestTesting {
+
+	private final String USER_AGENT = "Mozilla/5.0";
+
+	public static void main(String[] args) throws Exception {
+
+		RestTesting http = new RestTesting();
+
+		System.out.println("Testing 1 - Send Http GET request");
+		http.sendGet();
+
+		System.out.println("\nTesting 2 - Send Http POST request");
+		http.sendPost();
+
+	}
+
+	// HTTP GET request
+	private void sendGet() throws Exception {
+
+		String url = "https://www.resmed.com/au/en/index.html";
+
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+		// optional default is GET
+		con.setRequestMethod("GET");
+
+		int responseCode = con.getResponseCode();
+		System.out.println("\nSending 'GET' request to URL : " + url);
+		System.out.println("Response Code : " + responseCode);
+		
+	}
+
+	// HTTP POST request
+	private void sendPost() throws Exception {
+
+		String POST_URL ="http://localhost:9090/SpringMVC/home";
+		URL obj = new URL(POST_URL);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		con.setRequestMethod("POST");
+		con.setRequestProperty("User-Agent", USER_AGENT);
+
+		// For POST only - START
+		con.setDoOutput(true);
+		OutputStream os = con.getOutputStream();
+		os.write(POST_PARAMS.getBytes());
+		os.flush();
+		os.close();
+		// For POST only - END
+
+		int responseCode = con.getResponseCode();
+		System.out.println("POST Response Code :: " + responseCode);
+
+		if (responseCode == HttpURLConnection.HTTP_OK) { //success
+			BufferedReader in = new BufferedReader(new InputStreamReader(
+					con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+
+			// print result
+			System.out.println(response.toString());
+		} else {
+			System.out.println("POST request not worked");
+		}
+	}
+
+
+	}
+
+}
+
